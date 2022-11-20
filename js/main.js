f81342bda20e5963fd036b580d18ca7cd9004210
Yriy87 <106962025+Yriy87@users.noreply.github.com>
2022-11-20 20:00:23 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+  $('.bike-slider, .slider__items').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $(".menu, .bike__column").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+
+});
